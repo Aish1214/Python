1d55b999a899fa2fb649626d84e36bc10217c2c6
Aish1214 <funaguskaraishwarya@gmail.com>
2026-01-28 21:47:16 +0530

    Add files via upload
    It's about Comments, more about print function and escape sequence

---
+++ p2.py
@@ -0,0 +1,15 @@
+#it is abt comments, escape sequence and print command 
+#Author: Aishwarya F
+#1. Escape Sequence
+print("this is a a boy \"cute as\" Harish")
+
+print("this is a boy and\n she is a girl")
+
+#2. Comments 
+' ' 'comment single line' ' '
+"""Multi Line comment"""
+
+#3. Print 
+print("this is a boy", 4, 5)
+print("9", "10", sep=" and ") # separate multiple strings with the adjoiner and or any other as per requirement 
+print("9", "10", end=" 009 ") # it should end with the specified number in the end context box 
