aadb1abd10e9c3c1ecf17228d8d7f9af9ef4a9d3
Aish1214 <funaguskaraishwarya@gmail.com>
2026-01-31 11:18:14 +0530

    Add files via upload
    Simple calculator

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+
+
+#calculator 
+a = int(input("Please enter the number: "))
+print("your first number is ", a)
+
+b = int(input("Please another the number: "))
+print("your second number is ", b)
+
+print("Which function you would choose?")
+print("add","sub","div","mul") 
+print("Kindly mention below")
+e = input()
+add = print("The sum is, " ,a+b)
+sub = print("the Difference is ", a-b)
+div = print("the product is " , a/b)
+
+
