6d552221b1d8dc49fda222b9e03e62f7fd16e14d
Aish1214 <funaguskaraishwarya@gmail.com>
2026-01-31 15:25:40 +0530

    Taking_user's_input

---
+++ Taking_user's_input.py
@@ -0,0 +1,12 @@
+#2 ways of taking the input 
+
+a = input()
+print(a)
+
+#second way is consider addition and also we need to remember that if the data type is not specified then the value is returned as string not as any input's type of data type 
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ") 
+
+#print the number add the following 
+print(int(a) + int(b))
