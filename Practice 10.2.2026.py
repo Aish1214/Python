78491fa9f30871003180ba834fd812d61cfaf1a9
Aish1214 <funaguskaraishwarya@gmail.com>
2026-02-10 14:00:21 +0530

    Add files via upload
    Here as it was long since I practiced the Python due to my illness I took 5 days rest 
    today have again resumed the python practice 
    hence here i have practiced the input function where i left the one earlier

---
+++ Practice 10.2.2026.py
@@ -0,0 +1,14 @@
+#Practising Input function
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+print(a + b)
+
+
+#second way 
+a = input("Enter a first number: ")
+b = input("Enter a second number: ")
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
